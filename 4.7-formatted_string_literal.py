8e58ce78606322f503d0db79ff54a0bba5d19e4c
akaMiWP <bangkokappdevelopment@gmail.com>
2025-09-18 18:23:15 +0700

    chore: learnt formatted string literal

---
+++ 4.7-formatted_string_literal.py
@@ -0,0 +1,18 @@
+def first_exercise():
+    weight = 0.2
+    animal = "newt"
+    print(str(weight) + " kg is the weight of the " + str(animal))
+
+def second_exercise():
+    weight = 0.2
+    animal = "newt"
+    print("{} kg is the weight of the {}".format(weight, animal))
+
+def third_exercise():
+    weight = 0.2
+    animal = "newt"
+    print(f"{weight} kg is the weight of the {animal}")
+
+first_exercise()
+second_exercise()
+third_exercise()
